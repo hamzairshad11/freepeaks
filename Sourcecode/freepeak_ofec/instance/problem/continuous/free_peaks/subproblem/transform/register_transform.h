865d75bc3efd46eecd24e8bf2750a0714fb3cc68
yiya diao <diaoyiya@outlook.com>
2026-02-04 21:03:27 +0800

    update

--- Sourcecode/freepeak_ofec/instance/problem/continuous/free_peaks/subproblem/transform/register_transform.h
+++
@@ -1,8 +0,0 @@
-#ifndef OFEC_FREE_PEAKS_REGISTER_TRANSFORM_H
-#define OFEC_FREE_PEAKS_REGISTER_TRANSFORM_H
-
-namespace ofec::free_peaks {
-	void registerTransform();
-}
-
-#endif // !OFEC_FREE_PEAKS_REGISTER_TRANSFORM_H
